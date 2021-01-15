82c32b556cb4f19dc2cbcfe686a6c51fb54f6f4d
therealkundanpatil <kp28021996@gmail.com>
2021-01-15 12:07:25 -0500

    -Use the modifed component in the main application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+import Cards from "./components/Cards/Cards.jsx";
+import Chart from "./components/Chart/Chart.jsx";
+import CountryPicker from "./components/CountryPicker/CountryPicker.jsx";
+
+
 function App(){
     return(
-        <h1>
-            App
-        </h1>
+        <div>
+            <div>
+                <h1>
+                    App
+                </h1>
+            </div>
+            <Cards/>
+            <CountryPicker/>
+            <Chart/>
+        </div>
+        
     );
 }
 
