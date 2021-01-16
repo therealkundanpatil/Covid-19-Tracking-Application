51c1cac2a59c190a615a146009118a99e0585fa5
therealkundanpatil <kp28021996@gmail.com>
2021-01-15 22:55:16 -0500

    -Fix bug that prevented the state update

--- src/App.js
+++ src/App.js
@@ -16,21 +16,22 @@ class App extends React.Component {
 
     async componentDidMount() {
         const fetchedData = await fetchData();
-        this.setState({ data: fetchedData })
+        this.setState({ data: fetchedData });
     }
 
-    async handleCountryChange(country){
-        console.log(country);
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
     }
 
     render() {
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
                 <Cards data={data} />
-                <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
             </div>
         )
     }
